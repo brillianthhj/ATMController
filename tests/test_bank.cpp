5d305c9002769d1d767fc8371db07ce0b2385185
brillianthhj <brillianthhj@gmail.com>
2026-08-11 23:08:38 +0900

    complete test_account, doiong test_bank

---
+++ tests/test_bank.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "Bank.h"
+
+class BankTestFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    Bank bank;
+};
+
+TEST_F(BankTestFixture, IsAccountNullPtr) {
+
+
+}
+
+TEST_F(BankTestFixture, getAccount) {
+
+}
+
+TEST_F(BankTestFixture, verifyAccountAndPinNum) {
+
+
+}
